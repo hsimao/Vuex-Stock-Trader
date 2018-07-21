3c877150e3a8947f2972e934d5a238ab2d0ce34c
hsimao <e087754958@gmail.com>
2018-07-21 12:53:25 +0800

    add global sass

--- build/utils.js
+++ build/utils.js
@@ -54,16 +54,53 @@ exports.cssLoaders = function (options) {
     }
   }
 
-  // https://vue-loader.vuejs.org/en/configurations/extract-css.html
+
+
+  //////////////
+  // == sass 全域設置 Start
+  function resolveResouce(name) {
+    return path.resolve(__dirname, '../src/css/style.sass');
+  }
+
+  function generateSassResourceLoader() {
+    let loaders = [
+      cssLoader,
+      // 'postcss-loader',
+      {
+        loader: 'sass-loader',
+        options: {
+          indentedSyntax: true
+        }
+      },
+      {
+        loader: 'sass-resources-loader',
+        options: {
+          // it need a absolute path
+          resources: resolveResouce('../src/css/style.sass')
+        }
+      }
+    ];
+    if (options.extract) {
+      return ExtractTextPlugin.extract({
+        use: loaders,
+        fallback: 'vue-style-loader'
+      })
+    } else {
+      return ['vue-style-loader'].concat(loaders)
+    }
+  }　　
+
   return {
     css: generateLoaders(),
     postcss: generateLoaders(),
     less: generateLoaders('less'),
-    sass: generateLoaders('sass', { indentedSyntax: true }),
-    scss: generateLoaders('sass'),
+    sass: generateSassResourceLoader(),
+    scss: generateSassResourceLoader(),
     stylus: generateLoaders('stylus'),
     styl: generateLoaders('stylus')
   }
+  // == sass 全域設置 End
+  //////////////
 }
 
 // Generate loaders for standalone style files (outside of .vue)
